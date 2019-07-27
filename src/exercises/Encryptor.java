19233ebdc464f32c876fab1bb3411dc90652b6ae
Kolinjo <kozicmarko@yahoo.com>
2019-07-28 01:15:44 +0200

    Added code

--- src/exercises/Encryptor.java
+++ src/exercises/Encryptor.java
@@ -12,5 +12,18 @@ public class Encryptor {
 		}
 		return builder.toString();
 	}
+	
+	public static String decrypt (String word) {
+		
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < word.length(); i++) {
+			int numericalValue = word.charAt(i);
+			numericalValue -=2;
+			builder.append((char)numericalValue);
+			
+		}
+		
+		return builder.toString();
+	}
 
 }
