3025390251733a526b0875f5f282cc8bc6ceacc4
Kolinjo <kozicmarko@yahoo.com>
2019-07-28 01:17:22 +0200

    Added code

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -9,4 +9,27 @@ public class Palindrome {
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+	
+	
+	public boolean isWordInDictonary (String word) {
+		
+		List<String> array = new Palindrome().loadWords();
+		
+		if (array.contains(word)) {
+			return true;
+		}else {
+		
+		return false;
+		}
+	}
+	
+	public boolean isPalindrome (String word) {
+		char array []= word.toCharArray();
+		for (int i = 0; i < array.length/2; i++) {
+			if (array[i]!=array[array.length-i-1]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
